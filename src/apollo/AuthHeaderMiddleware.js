447857aa825080a1fbf6d5bf3aea78ce9221a61d
meeken1998 <78071836@qq.com>
2019-09-22 20:39:04 +0800

    debug

--- src/apollo/AuthHeaderMiddleware.js
+++ src/apollo/AuthHeaderMiddleware.js
@@ -4,7 +4,7 @@ export default function (req, next) {
   }
 
   const token = localStorage.getItem('token')
-  let headers = JSON.stringify(localStorage.getItem('headers'))
+  let headers = JSON.parse(localStorage.getItem('headers'))
   req.options.headers.authorization = token ? `Bearer ${token}` : null
   if (typeof headers == 'object') {
     for (let key in headers) {
