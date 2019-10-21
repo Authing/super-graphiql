06bfbc4806188cd2c7fe2d2c887acd7eeb513bbd
廖长江 <changjiangthecoder@gmail.com>
2019-10-21 12:09:32 +0800

    feature: update getClientWhenSdkInit doc

--- src/store/apollo.js
+++ src/store/apollo.js
@@ -5,9 +5,34 @@ const state = {
     historyList: [],
     nowHistory: 0,
     apiDocs: {
+
         getClientWhenSdkInit: {
             name: '初始化',
-            brief: '若你需要管理用户池或用户，请先从此方法获取 accessToken，后续的管理用户或用户池的操作请发送此 Token，在下文中，此 Token 命名为「OwnerToken」。',
+            brief: `
+1. 为什么需要用户池鉴权？
+> 出于安全性考虑，进行删除用户、创建角色等操作前需要先获取 accessToken。  
+
+2. 如何获取 accessToken？
+> 需要两个参数
+- \`clientId\`
+- \`secret\`
+
+3. 如何获取 \`clientId\` 和 \`secret\` ?
+
+- 前往 [authing.cn](https://authing.cn/) 注册账号
+- [新建用户池](https://docs.authing.cn/authing/quickstart/hello-world)
+- 通过 用户池 -> 基础配置 -> 基础配置 找到 \`clientId\` 和 \`secret\`
+
+4. 返回数据字段
+
+- \`accessToken\`
+- \`clientInfo\`: 用户池的详细信息
+
+5. 如何使用 \`accessToken\` ?
+
+> 添加到 HTTP 请求的 [Authorization Header](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Authorization) 中
+
+            `,
             type: '用户池鉴权'
         },
         register: {
