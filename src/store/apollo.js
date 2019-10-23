8fe8ad2f84ed730506f9daec0f632545772a9969
廖长江 <changjiangthecoder@gmail.com>
2019-10-23 19:26:55 +0800

    文档：获取用户列表

--- src/store/apollo.js
+++ src/store/apollo.js
@@ -212,7 +212,15 @@ Authing 提供给开发者“一键关闭注册”选项，可在后台 **基础
         },
         users: {
             name: '获取用户列表',
-            brief: '此接口用来读取用户列表，建议使用 OwnerToken',
+            brief: `
+获取用户列表。支持分页。用户信息支持基础信息和附带扩展字段两种模式。
+1. 部分请求参数说明
+- registerInClient：用户池 ID
+- page: 第几页。
+- count：一页包含用户数目，默认为10，最大为200。
+- populate：布尔值。默认为false, 如果为true,将会返回用户的扩展字段：group、clientType、userLocation 、userLoginHistory、systemApplicationType（详情见”读取用户资料”接口）。
+
+            `,
             type: '用户管理'
         },
         checkLoginStatus: {
