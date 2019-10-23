bbe9daa7f8c93c6b27bd3c87481d6aba1a4ecb82
廖长江 <changjiangthecoder@gmail.com>
2019-10-23 19:24:37 +0800

    文档：读取用户资料

--- src/store/apollo.js
+++ src/store/apollo.js
@@ -101,17 +101,113 @@ Authing 提供给开发者“一键关闭注册”选项，可在后台 **基础
         },
         decodeJwtToken: {
             name: '解析 JWT Token',
-            brief: '此接口用来解析 JWT Token。',
+            brief:  `
+解析 JWT Token。
+1. 点击 [Authing官方文档](https://learn.authing.cn/authing/advanced/authentication/jwt-token) 了解更多 JWT Token 相关内容。
+2. 解析成功示例
+\`\`\`
+{
+    "data": {
+        "decodeJwtToken": {
+            "data": {
+                "email": "1066983132@qq.com",
+                "id": "5da7ed9950396c1ad9623787",
+                "clientId": "59f86b4832eb28071bdd9214",
+                "unionid": null
+            },
+            "status": {
+                "message": "token 解析正常",
+                "code": 200,
+                "status": null
+            },
+            "iat": "1571801657",
+            "exp": 1573097657
+        }
+    }
+}
+
+\`\`\`
+部分字段说明：
+- email: 用户邮箱
+- id: 用户ID
+- clientId: 用户池ID
+- unionid
+- iat: 签名时间戳
+- exp: 签名过期时间戳
+
+3. 解析失败示例
+\`\`\`
+{
+    "data": {
+        "decodeJwtToken": {
+        "data": null,
+        "status": {
+            "message": "token 错误",
+            "code": 2207,
+            "status": null
+        },
+        "iat": null,
+        "exp": null
+        }
+    }
+}
+\`\`\`
+
+`,
             type: '用户鉴权'
         },
         refreshToken: {
             name: '刷新 Token',
-            brief: '此接口用来刷新 Token，Token 是用户登录的凭证',
+            brief: `
+刷新指定用户的 JWT token。
+1. 此 JWT token 有什么用？
+
+每个用户都有唯一对应的 JWT token, 作为登陆凭证。
+
+2. 参数说明
+- client: 用户池 ID
+- user: 用户 ID
+
+3. 返回数据示例
+\`\`\`
+{
+    "data": {
+      "refreshToken": {
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImlkIjoiNWRhN2VkYWI1MDM5NmMxYWQ5NjIzNzhjIn0sImlhdCI6MTU3MTgxMDY5MSwiZXhwIjoxNTczMTA2NjkxfQ.obN2YMjasbew8ORhd_IxALTVoFSs0PVm-JQH2ULhBP0",
+        "iat": 1571810691,
+        "exp": 1573106691
+      }
+    }
+}
+\`\`\`
+
+部分字段说明：
+- token: 刷新过后的 token
+- iat: 签名时间戳
+- exp: 签名过期时间戳（默认为签名时间过后15天）
+
+            `,
             type: '用户鉴权'
         },
         user: {
             name: '读取用户资料',
-            brief: '获取用户资料，建议使用 OwnerToken。\n支持批量获取用户资料，参见 https://docs.authing.cn/authing/sdk/open-graphql#du-qu-yong-hu-zi-liao',
+            brief: `
+读取用户资料。同时支持用户 ID 和用户登陆凭证读取。
+1. 部分请求参数说明
+- 可选参数：
+    - registerInClient: 用户池 ID。
+    - id: 用户id。
+    - token: 该用户（**非开发者**）的登陆凭证，如果指定，将会忽略 id 字段。
+    - auth: 布尔值
+    - userLoginHistoryPage: 用户登陆记录分页数。
+    - userLoginHistoryCount: 用户登陆记录每页数目。
+
+2. 返回结果部分字段说明
+- group: 所在群组
+- clientType: 用户池类型
+- userLocation: 用户地址
+- userLoginHistory: 用户历史登陆记录。
+            `,
             type: '用户管理'
         },
         users: {
